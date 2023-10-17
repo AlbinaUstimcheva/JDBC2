3bf44bda788ed64ee66a903800790ee490d35b1c
AlbinaUstimcheva <123832033+AlbinaUstimcheva@users.noreply.github.com>
2023-10-17 14:08:38 +0200

    feat: jpa-repository + CRUD

--- src/main/java/ru/netology/javadata_4_hibernate/service/Service.java
+++ src/main/java/ru/netology/javadata_4_hibernate/service/Service.java
@@ -1,19 +1,40 @@
 package ru.netology.javadata_4_hibernate.service;
 
 import ru.netology.javadata_4_hibernate.entity.Person;
-import ru.netology.javadata_4_hibernate.repository.Repository;
+import ru.netology.javadata_4_hibernate.repository.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @org.springframework.stereotype.Service
 public class Service {
-    private final Repository repository;
+    private final PersonRepository repository;
 
-    public Service(Repository repository) {
+    public Service(PersonRepository repository) {
         this.repository = repository;
     }
 
     public List<Person> getPersonsByCity(String city) {
-        return repository.getPersonsByCity(city);
+        return repository.findByCity(city);
+    }
+
+    public List<Person> getPersonsByAgeLessThanOrderBy(int age) {
+        return repository.findByAgeLessThanOrderByAge(age);
+    }
+
+    public Optional<Person> getPersonByNameAndSurname(String name, String surname) {
+        return repository.findByNameAndSurname(name, surname);
+    }
+
+    public Person save(Person person) {
+        return repository.save(person);
+    }
+
+    public Long count() {
+        return repository.count();
+    }
+
+    public void deleteAllPersons(List<Person> personList) {
+        repository.deleteAll(personList);
     }
 }
